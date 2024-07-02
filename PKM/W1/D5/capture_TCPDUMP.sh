124431c1023841ec09ba8f9beb782f6a07230729
Fred <96602008+FredericGariepy@users.noreply.github.com>
2024-07-02 06:55:46 -0400

    Update capture_TCPDUMP.sh

--- PKM/W1/D5/capture_TCPDUMP.sh
+++ PKM/W1/D5/capture_TCPDUMP.sh
@@ -2,7 +2,7 @@
 
 # Function to start tcpdump in the background
 start_tcpdump() {
-    sudo tcpdump -i eth0 host <ip_address> -w capture.pcap &
+    sudo tcpdump -i eth0 host <ip_address> -w capture.pcap  # The '&' ensures that tcdump PID is captured instead of bg PID
     # Store the PID of the tcpdump process
     TCPDUMP_PID=$!
 }
@@ -11,13 +11,13 @@ start_tcpdump() {
 stop_tcpdump() {
     # Kill the tcpdump process
     sudo kill $TCPDUMP_PID
-
     # Here there should be added a check for the kill status of TCPDUMP_PID
+    # Also some kill options dependng on TCPDUMP_PID status
 }
 
 # Main script execution
 echo "Starting tcpdump capture..."
-start_tcpdump
+start_tcpdump # call function
 
 # Execute your command here (replace with your actual command)
 echo "Executing command..."
@@ -26,6 +26,6 @@ echo "Executing command..."
 
 # Stop tcpdump after command execution
 echo "Stopping tcpdump..."
-stop_tcpdump
+stop_tcpdump # call function
 
 echo "Capture complete."
