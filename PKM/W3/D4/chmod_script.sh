65580fac10c2aa6361ee7e3c364fc5ff53f3f559
Fred <96602008+FredericGariepy@users.noreply.github.com>
2024-07-11 17:38:35 -0400

    Update chmod_script.sh

--- PKM/W3/D4/chmod_script.sh
+++ PKM/W3/D4/chmod_script.sh
@@ -1,7 +1,9 @@
 !/bin/bash
+# this is just a simple script to change a fil permission to 777
+# So in the VMs I can copy a bunch of logs and change their permisions
+# To use `sudo ./chmod_script.sh file1 file2 file3 file4 file_that_does_not_exist file5 file6 and so on...
 
 # $# is the provided args
-
 if [ $# -eq 0 ];
 
 then
@@ -13,7 +15,6 @@ exit 1
 fi
 
 # $@  means all argumnets
-
 for file in "$@"
 
 do
