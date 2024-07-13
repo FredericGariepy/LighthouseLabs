a228dede3fe11ab65098c5d8b9ccf9bb6a3aa0ac
Fred <96602008+FredericGariepy@users.noreply.github.com>
2024-07-13 15:25:35 -0400

    Create server_monitor.py

---
+++ PKM/W3/D5/backdoor/server_monitor.py
@@ -0,0 +1,18 @@
+import socket
+def Main():
+  host = socket.gethostname()
+  port = 4444
+  serversocket = socket.socket()
+  serversocket.bind((host,port))
+  serversocket.listen(1)
+  print('Socket listening')
+
+
+  while True:
+     conn,addr = serversocket.accept()
+     print("Got connection from %s" % str(addr))
+     msg = 'Connecting Established'+ "\r\n"
+     conn.send(msg.encode('ascii'))
+     conn.close()
+if __name__ == '__main__':
+  Main()
