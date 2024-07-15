fe7a42afac22c73ebf568809ac4e691a2299ffb6
Fred <96602008+FredericGariepy@users.noreply.github.com>
2024-07-14 23:41:15 -0400

    Update triage.py

--- PKM/W3/D5/PROJECT_with_PAVAN/server-side/triage.py
+++ PKM/W3/D5/PROJECT_with_PAVAN/server-side/triage.py
@@ -1,13 +1,36 @@
 #!/usr/bin/python3
+
 import sys
+import re
 
 if len(sys.argv) < 2:
     print("Usage: triage_script.py <data>")
-    sys.exit(1)  # Exit the script with an error code
+    sys.exit(1)  # Exit the script with error code
 
+LOG_LINE_RegEx = r'^(?P<IP>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*\[(?P<timestamp>.*)\]\s"(?P<verb>[A-Z]+)\s(?P<path>[\w\/\.-]+)\s+(?P<protocol>[\w\/\.]+)"\s(?P<status_code>\d+)\s(?P<response_size>\d+).*'
+
+# data received from server.py (main loop)
 received_data = sys.argv[1]
 
 print("Triage: "+received_data)
-# data arrives here from the server.py file (main loop)
-# it should be regexed, analyzed, deposited.
-                                 
+
+def regex_parse(log_line):
+    match = re.match(LOG_LINE_RegEx, log_line)
+    if match:
+        return match.groupdict()
+    else:
+        pass
+
+try:
+    log_line = regex_parse(received_data.strip())
+    print(log_line['IP'])
+    print(log_line['timestamp'])
+    print(log_line['verb'])
+    print(log_line['path'])
+    print(log_line['protocol'])
+    print(log_line['status_code'])
+    print(log_line['response_size'])
+
+    # use the above to make some logic
+except:
+    pass
