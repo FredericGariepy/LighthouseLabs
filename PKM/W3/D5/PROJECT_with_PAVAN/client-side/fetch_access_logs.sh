b150c04dc737a39bf07e8626c4c9ea159a63da46
Fred <96602008+FredericGariepy@users.noreply.github.com>
2024-07-14 23:27:38 -0400

    Update and rename PKM/W3/D5/PROJECT_with_PAVAN/fetch_access_logs.sh to PKM/W3/D5/PROJECT_with_PAVAN/client-side/fetch_access_logs.sh

--- PKM/W3/D5/PROJECT_with_PAVAN/client-side/fetch_access_logs.sh
+++ PKM/W3/D5/PROJECT_with_PAVAN/client-side/fetch_access_logs.sh
@@ -1,45 +1,17 @@
 #!/bin/bash
-
-
-
 log_dir="/var/log/apache2"
-
 client_script="/home/student/Documents/python_scripts/server_client/client.py"
-
-
-
 # Function to process new log files
-
 process_log() {
-
-
-
     local log_file="$1"
-
-
-
     last_line=$(tail -n 1 "$log_file")
-
-
-
     # feed last_line to python client script
-
     python3 "$client_script" "$last_line"
-
 }
-
-
-
 # Monitor log directory for new access.log files
-
 inotifywait -m -e create -e modify --format '%f' "$log_dir" |
-
 while read -r file; do
-
     if [[ "$file" == "access.log" ]]; then  #change this if condition to change the log fetching type (e.g. error, access)
-
         process_log "$log_dir/$file"
-
     fi
-
 done
