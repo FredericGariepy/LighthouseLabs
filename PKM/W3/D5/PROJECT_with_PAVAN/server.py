fbd119cc7408f276fcabe32e879c0f61f6852d48
Fred <96602008+FredericGariepy@users.noreply.github.com>
2024-07-14 23:15:22 -0400

    Update and rename SERVER.py to server.py

---
+++ PKM/W3/D5/PROJECT_with_PAVAN/server.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import socket
+import subprocess
+#import sys
+#import os
+
+# path to triage script
+path_triage_script='/home/student/Documents/python_scripts/server_client/triage_script.py'
+def Main():
+  host = '0.0.0.0' # listen all interfaces
+  port = 4444
+  serversocket = socket.socket()
+  serversocket.bind((host,port))
+  serversocket.listen(1)
+  print('Socket listening...')
+
+
+  while True:
+     conn,addr = serversocket.accept() 
+     #print("Accepted connection from  %s" % str(addr)) 
+    
+     
+     received = conn.recv(1024) # received bytes encoded
+     #print(f"Received {len(received)} bytes from {str(addr)}")
+
+     received_data=received.decode('ascii') # decoded data | access.log is type <'str'>
+     #print(received_data)
+     
+     #call the triage script with received decoded data
+     subprocess.run(['python3', path_triage_script, received_data])
+
+     # SEND ACK
+     #msg = 'Connecting Established'+ "\r\n"
+     #conn.send(msg.encode('ascii'))
+     conn.close()
+
+if __name__ == '__main__':
+  Main()
