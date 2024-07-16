a4badb7fa2ce86ee95e0e32c98d8101c9d4d26cd
Fred <96602008+FredericGariepy@users.noreply.github.com>
2024-07-16 17:39:01 -0400

    Update server.py

--- PKM/W3/D5/PROJECT_with_PAVAN/server-side/server-client/server.py
+++ PKM/W3/D5/PROJECT_with_PAVAN/server-side/server-client/server.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 import socket
 import threading
-
 from triage import TriageThread
+import multiprocessing
+import subprocess
 
+def start_log_monitor():
+    script_path = '/home/student/Documents/python_scripts/log_monitor_script/access_log_monitor.py'
+    subprocess.Popen(['python3', script_path], start_new_session=True)
 
 def main():
+    # Start the log monitor in a separate process
+    log_monitor_process = multiprocessing.Process(target=start_log_monitor)
+    log_monitor_process.start()
+
     host = '0.0.0.0'  # listen on all interfaces
     port = 4444
     serversocket = socket.socket()
     serversocket.bind((host, port))
     serversocket.listen(1)
     print('Socket listening...')
-
+    
     while True:
         conn, addr = serversocket.accept()
         received = conn.recv(1024)
