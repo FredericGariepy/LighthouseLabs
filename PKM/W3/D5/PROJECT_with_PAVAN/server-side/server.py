c72e02242758623f4e06a197f0bcdb756260d825
Fred <96602008+FredericGariepy@users.noreply.github.com>
2024-07-16 11:24:22 -0400

    Update server.py (refactor with threads)
    refactored with threads

--- PKM/W3/D5/PROJECT_with_PAVAN/server-side/server.py
+++ PKM/W3/D5/PROJECT_with_PAVAN/server-side/server.py
@@ -1,40 +1,28 @@
 #!/usr/bin/python3
 import socket
-import subprocess
-#import sys
-#import os
+import threading
 
-# NOTE!: change this path to your triage script path
-# path to triage script
-path_triage_script='/home/student/Documents/python_scripts/server_client/triage.py'
-
-def Main():
-  host = '0.0.0.0' # listen all interfaces
-  port = 4444
-  serversocket = socket.socket()
-  serversocket.bind((host,port))
-  serversocket.listen(1)
-  print('Socket listening...')
+from triage import TriageThread
 
 
-  while True:
-     conn,addr = serversocket.accept() 
-     #print("Accepted connection from  %s" % str(addr)) 
-    
-     
-     received = conn.recv(1024) # received bytes encoded
-     #print(f"Received {len(received)} bytes from {str(addr)}")
+def main():
+    host = '0.0.0.0'  # listen on all interfaces
+    port = 4444
+    serversocket = socket.socket()
+    serversocket.bind((host, port))
+    serversocket.listen(1)
+    print('Socket listening...')
 
-     received_data=received.decode('ascii') # decoded data | access.log is type <'str'>
-     #print(received_data)
-     
-     #call the triage script with received decoded data
-     subprocess.run(['python3', path_triage_script, received_data])
-
-     # SEND ACK
-     #msg = 'Connecting Established'+ "\r\n"
-     #conn.send(msg.encode('ascii'))
-     conn.close()
+    while True:
+        conn, addr = serversocket.accept()
+        received = conn.recv(1024)
+        received_data = received.decode('ascii')
+        
+        # Create and start a new TriageThread for each connection
+        triage_thread = TriageThread(received_data)
+        triage_thread.start()
+        
+        conn.close()
 
 if __name__ == '__main__':
-  Main()
+    main()
