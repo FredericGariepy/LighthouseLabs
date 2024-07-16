c45dd3ee899a14287c30b14197b9f989f13e3bc2
Fred <96602008+FredericGariepy@users.noreply.github.com>
2024-07-16 11:23:33 -0400

    Update triage.py (refactor with threads)
    refactored with threads

--- PKM/W3/D5/PROJECT_with_PAVAN/server-side/triage.py
+++ PKM/W3/D5/PROJECT_with_PAVAN/server-side/triage.py
@@ -1,92 +1,112 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 
-# The purpose of this script is to receive raw client logs from server.py
-# 1. logs are received
-# 2. logs are parsed
-# 3. logs are categorized/stored (In the real world, this should be done in a database)
-
-# impports
-import sys
+import threading
 import re
+import sys
 
-if len(sys.argv) < 2:
-    print("Usage: triage_script.py <data>")
-    sys.exit(1)  # Exit the script with error code
+class TriageThread(threading.Thread):
+    
+    # triage constructor, called when class instantiated 
+    def __init__(self, log_line):
+        
+        # constructor of the parent Thread class
+        threading.Thread.__init__(self)
+        
+        # class var
+        self.log_line = log_line.strip()
 
-# regex parses & formats into dictionary object {?P<key> : value}
-# Regular expressions for parsing Apache error and access logs
+    # When you call start() on a thread, it automatically calls this run() method
+    def run(self):
+        self.match_log_line(self.log_line)
 
+    # file write paths
+    urgent_error_log_path='/home/student/Documents/log_storage/error_logs/urgent_error.log'
+    standard_error_log_path='/home/student/Documents/log_storage/error_logs/standard_error.log'
+    urgent_access_log_path='/home/student/Documents/log_storage/access_logs/urgent_access.log'
+    standard_access_log_path='/home/student/Documents/log_storage/access_logs/standard_access.log'
+    standard_unknown_log_path='/home/student/Documents/log_storage/unknown_logs/standard_unknown.log'
 
-regex_apache2_error_log = r'^(?P<date>\[\w+\s\w+\s\d+\s\d+:\d+:\d+\.\d+\s\d+\])\s(?P<loglevel>\[\w+:\w+\])\s(?P<pid>\[pid\s\d+:tid\s\d+\])\s(?P<message>.*)'
-regex_apache2_access_log =r'^(?P<IP>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+-\s+-\s+\[(?P<timestamp>[^\]]+)\]\s+"(?P<verb>\w+)\s+(?P<path>[^"]+)\s+(?P<protocol>HTTP/[\d\.]+)"\s+(?P<status_code>\d+)\s+(?P<response_size>[^"]\S*)\s+"(?P<referrer>[^"]*)"\s+"(?P<user_agent>[^"]*)'
-
-
-#-----------------------------------------------------
-
-
-# data received from server.py (main loop)
-received_data = sys.argv[1]
-log_line = received_data.strip()
-#-----------------------------------------------------
-
-# Support funtion: Parse with regex and return dictionary object
-def regex_parse(regex_expression, log_line):
-    match = re.match(regex_expression, log_line)
-    if match:
-        return match.groupdict()
-    else:
-        return None
-#-----------------------------------------------------
-
+    # regex parses & formats into dictionary object {?P<key> : value}
+    # Regular expressions for parsing Apache error and access logs
+    regex_apache2_error_log = r'^(?P<date>\[\w+\s\w+\s\d+\s\d+:\d+:\d+\.\d+\s\d+\])\s(?P<loglevel>\[\w+:\w+\])\s(?P<pid>\[pid\s\d+:tid\s\d+\])\s(?P<message>.*)'
+    regex_apache2_access_log =r'^(?P<IP>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+-\s+-\s+\[(?P<timestamp>[^\]]+)\]\s+"(?P<verb>\w+)\s+(?P<path>[^"]+)\s+(?P<protocol>HTTP/[\d\.]+)"\s+(?P<status_code>\d+)\s+(?P<response_size>[^"]\S*)\s+"(?P<referrer>[^"]*)"\s+"(?P<user_agent>[^"]*)'
 
 
+    # A static method is a method that belongs to the  class  rather than an instance of the class.
+    # It doesn't require the self parameter and can be called on the class itself
+    @staticmethod
+    def regex_parse(regex_expression, log_line):
+        match = re.match(regex_expression, log_line)
+        return match.groupdict() if match else None
 
-#-----------------------------------------------------
+    # thread funciton
+    def match_log_line(self, log_line):
+        matched = False
 
-# Main function: match the log line to a regex expression, store it in a file by sub category
-def match_log_line(log_line):
+        if not matched:
+            try:
+                access_log_line = self.regex_parse(self.regex_apache2_access_log, log_line)
+                if access_log_line:
+                    matched = True
+                    ##print(access_log_line)
+                    #print(access_log_line['IP'])
+                    #print(access_log_line['timestamp'])
+                    #print(access_log_line['verb'])
+                    #print(access_log_line['path'])
+                    #print(access_log_line['protocol'])
+                    #print(access_log_line['status_code'])
+                    #print(access_log_line['response_size'])
+                    #print(access_log_line['referrer'])
+                    #print(access_log_line['user_agent'])
+                    print('access log detected')
 
-    matched = False
+                    status_code = access_log_line['status_code']
 
-    if not matched:
-    # Parse as Apache access log (most frequent)
-        try:
-            access_log_line = regex_parse(regex_expression=regex_apache2_access_log, log_line=log_line)
-            if access_log_line:
-                matched = True
-                ##print(access_log_line)
-                #print(access_log_line['IP'])
-                #print(access_log_line['timestamp'])
-                #print(access_log_line['verb'])
-                #print(access_log_line['path'])
-                #print(access_log_line['protocol'])
-                #print(access_log_line['status_code'])
-                #print(access_log_line['response_size'])
-                #print(access_log_line['referrer'])
-                #print(access_log_line['user_agent'])
-        except:
-            pass
+                    important_status_codes = {'401', '403', '404', '500', '503', '504'}
 
-    if not matched:
-    # Parse as Apache error log
-        try:
-            error_log_line = regex_parse(regex_expression=regex_apache2_error_log, log_line=log_line)
-            if error_log_line:
-                matched = True
-                #print(error_log_line) 
-                #print(error_log_line['date'])
-                #print(error_log_line['loglevel'])
-                #print(error_log_line['pid'])
-                #print(error_log_line['message'])
-        except:
-            pass
+                    if status_code in important_status_codes:
+                        print('urgent log detected')
+                        with open(self.urgent_access_log_path, 'a') as f:
+                            f.write(log_line + '\n')
+                    else:
+                        with open(self.standard_access_log_path, 'a') as f:
+                            f.write(log_line + '\n')
+            except:
+                pass
 
+        if not matched:
+            try:
+                error_log_line = self.regex_parse(self.regex_apache2_error_log, log_line)
+                if error_log_line:
+                    matched = True
+                    #print(error_log_line)
+                    #print(error_log_line['date'])
+                    #print(error_log_line['loglevel'])
+                    #print(error_log_line['pid'])
+                    #print(error_log_line['message'])
+                    print('error log detected')
 
-    if not matched:
-        pass
-        #print(log_line)
+                    if 'error' in error_log_line['loglevel']:
+                        print('urgent error log detected')
+                        with open(self.urgent_error_log_path, 'a') as f:
+                            f.write(log_line + '\n')
+                    else:
+                        with open(self.standard_error_log_path, 'a') as f:
+                            f.write(log_line + '\n')
+            except:
+                pass
 
-#-----------------------------------------------------
-#print(log_line)
-match_log_line(log_line)
-#-----------------------------------------------------
+        if not matched:
+            with open(self.standard_unknown_log_path, 'a') as f:
+                f.write(log_line + '\n')
+
+# This part is to run triage.py standalone
+#if __name__ == '__main__':
+#    if len(sys.argv) < 2:
+#        print("Usage: triage.py <data>")
+#        sys.exit(1)
+#    
+#    log_line = sys.argv[1]
+#    triage_thread = TriageThread(log_line)
+#    triage_thread.start()
+#    triage_thread.join()
